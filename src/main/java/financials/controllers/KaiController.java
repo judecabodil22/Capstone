782473b5cd69e5f54e8b0e11e3038876fd38a489
Abijah Molina <jharnmolina@gmail.com>
2017-09-21 03:44:11 +0800

    Select Functions

--- src/main/java/financials/controllers/KaiController.java
+++ src/main/java/financials/controllers/KaiController.java
@@ -1,6 +1,7 @@
 package financials.controllers;
 
 import java.io.Console;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,10 +20,15 @@ public class KaiController {
 	private KaiDao AbiDao;
 
 	@RequestMapping("/Collections")
-	public ModelAndView Kai(@ModelAttribute("modelKai") KaiModel Abi) {
+	public ModelAndView Kai(@ModelAttribute("modelKai") KaiModel Abi)
+	{
+		List<KaiModel> KaiList= AbiDao.getUsers();
 		ModelAndView c_view = new ModelAndView();
 		c_view.addObject("Welcome", "Hello Bish!");
 		c_view.setViewName("Collections/Kai");
+		c_view.addObject("List",KaiList);
+		
+		
 
 		return c_view;
 	}
@@ -31,7 +37,9 @@ public class KaiController {
 	public ModelAndView save(@ModelAttribute("modelKai") KaiModel Abi,
 			@RequestParam(value = "Save", required = false) String Save,
 			@RequestParam(value = "Update", required = false) String Update,
-			@RequestParam(value = "Delete", required = false) String Delete)
+			@RequestParam(value = "Delete", required = false) String Delete,
+			@RequestParam(value = "Select", required = false) String SelectAll,
+			@RequestParam(value = "ID", required = false) String Select)
 	{
 		ModelAndView c_view;
 		c_view = new ModelAndView();
@@ -41,15 +49,24 @@ public class KaiController {
 				
 		if(Save!=null)
 		{
-			bool= AbiDao.c_insert(Abi);
+			bool= AbiDao.insert(Abi);
 		}
 		else if (Update!=null)
 		{
-			bool= AbiDao.c_update(Abi);
+			bool= AbiDao.update(Abi);
 		}
 		else if(Delete!=null)
 		{
-			bool= AbiDao.c_delete(Abi);
+			bool= AbiDao.delete(Abi);
+		}
+		else if(SelectAll!=null)
+		{
+			AbiDao.getUsers();
+		}
+		else if(Select!=null)
+		{
+			List<KaiModel> list = AbiDao.findByIDList(Abi);
+			c_view.addObject("List",list);
 		}
 		
 		
