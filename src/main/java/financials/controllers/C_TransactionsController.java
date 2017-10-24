f84ef9c79b64eae7f8465f242c857485c3bf7274
Abijah Molina <jharnmolina@gmail.com>
2017-10-25 05:26:53 +0800

    Create Func
    May errors pa ata yung iba for codes lang muna. HAHAHHAHA dont bash
    sakalen ko kayo

--- src/main/java/financials/controllers/C_TransactionsController.java
+++ src/main/java/financials/controllers/C_TransactionsController.java
@@ -1,17 +1,38 @@
 package financials.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import financials.dao.C_TransactionsDao;
+import financials.model.C_TransactionsModel;
 
 @Controller
 public class C_TransactionsController {
 
+	
+	@Autowired
+	C_TransactionsDao a;
+	
 	@RequestMapping("financials_ctransac") //url
-	public ModelAndView transactions()
+	public ModelAndView transactions(@ModelAttribute("ctransac")C_TransactionsModel ctransac)
 	{
 		ModelAndView ts = new ModelAndView();
 		ts.setViewName("Collections/C_Transactions");
+		
+		List<C_TransactionsModel> list = a.dropDownType();
+		ts.addObject("IGP_Type", list);
+		
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i).getC_Type());
+		}
 		return ts;
+	
 	}
+	
+	
 }
