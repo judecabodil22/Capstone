1b030b85ee425aa4af420309311c8c0748b4b76c
Abijah Molina <jharnmolina@gmail.com>
2017-10-28 01:42:12 +0800

    Create?
    Di ko sure if gumagana

--- src/main/java/financials/controllers/C_TransactionsController.java
+++ src/main/java/financials/controllers/C_TransactionsController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import financials.dao.C_TransactionsDao;
 import financials.model.C_TransactionsModel;
+import financials.model.apar_CreateapMODEL;
 
 @Controller
 public class C_TransactionsController {
@@ -32,6 +34,24 @@ public class C_TransactionsController {
 		}
 		return ts;
 	
+	}
+	@RequestMapping("ctrans_save")
+	public ModelAndView save(@ModelAttribute("ctransac") C_TransactionsModel trans,
+			@RequestParam(value = "ohyes", required = false) String save)
+	{
+		ModelAndView ts = new ModelAndView();
+		
+	
+		
+		ts.setViewName("financials_ctransac");
+/*		if(save != null)*/
+	/*	{*/
+			a.insert(trans);
+			System.out.println("Success");
+			List<C_TransactionsModel> list = a.view();
+			ts.addObject("list", list);
+	/*	}*/
+		return ts;
 	}
 	
 	
--- src/main/java/financials/model/C_TransactionsModel.java
+++ src/main/java/financials/model/C_TransactionsModel.java
@@ -1,13 +1,16 @@
 package financials.model;
 
+import java.sql.Date;
+
 public class C_TransactionsModel {
 	
 	private int C_OR_ID;
+	private String C_OR_No;
 	private String C_Type;
 	private String C_Payee;
 	private String C_TransDesc;
 	private Float C_Payment;
-	private String C_Date_Paid;
+	private Date C_Date_Paid;
 	private int Type_ID;
 	
 	public int getC_OR_ID() 
@@ -41,17 +44,24 @@ public class C_TransactionsModel {
 	public void setC_Payment(Float c_Payment) {
 		C_Payment = c_Payment;
 	}
-	public String getC_Date_Paid() {
-		return C_Date_Paid;
-	}
-	public void setC_Date_Paid(String c_Date_Paid) {
-		C_Date_Paid = c_Date_Paid;
-	}
+	
 	public int getType_ID() {
 		return Type_ID;
 	}
 	public void setType_ID(int type_ID) {
 		Type_ID = type_ID;
+	}
+	public Date getC_Date_Paid() {
+		return C_Date_Paid;
+	}
+	public void setC_Date_Paid(Date c_Date_Paid) {
+		C_Date_Paid = c_Date_Paid;
+	}
+	public String getC_OR_No() {
+		return C_OR_No;
+	}
+	public void setC_OR_No(String c_OR_No) {
+		C_OR_No = c_OR_No;
 	}
 	
 	
