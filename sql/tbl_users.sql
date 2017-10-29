cbe300da4530232a09af12ee64d9e31da0675fe7
jonathan.duran_310@yahoo.com <jonathan.duran_310@yahoo.com>
2017-10-29 14:20:50 +0800

    SQL
    Please put SQL files here.
    all.sql includes all the tables.
    Also create a file for each table

---
+++ sql/tbl_users.sql
@@ -0,0 +1,29 @@
+/*    ==Scripting Parameters==
+
+    Source Server Version : SQL Server 2016 (13.0.4001)
+    Source Database Engine Edition : Microsoft SQL Server Express Edition
+    Source Database Engine Type : Standalone SQL Server
+
+    Target Server Version : SQL Server 2016
+    Target Database Engine Edition : Microsoft SQL Server Express Edition
+    Target Database Engine Type : Standalone SQL Server
+*/
+
+USE [Financials]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 10/17/2017 3:47:05 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[user_id] int NULL,
+	[username] [varchar](50) NULL,
+	[password] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+
