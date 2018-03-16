babfcd3be6fb16bcb7bfd02cee3d9ce740f56f34
judecabodil22 <judecabodil22@gmail.com>
2018-03-17 04:33:25 +0800

    Vianca AP List

--- src/main/java/financials/controllers/financials_ap_listController.java
+++ src/main/java/financials/controllers/financials_ap_listController.java
@@ -1,16 +1,24 @@
 package financials.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import financials.dao.CreateAPDAO;
+
 @Controller
 public class financials_ap_listController {
+	
+	@Autowired
+	CreateAPDAO apDao;
+	
 	
 	@RequestMapping("ap_list")//url
 	public ModelAndView fapl()
 	{
 		ModelAndView apl = new ModelAndView();
+		apl.addObject("APList",apDao.getcap());
 		apl.setViewName("AP_AR/apar_ap_list");
 		return apl;
 	}
