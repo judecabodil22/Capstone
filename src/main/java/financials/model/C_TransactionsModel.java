b7709038b8b7bbaa2ea049fb5e9f921fae65749c
viancalee <31964861+vbrdplee@users.noreply.github.com>
2018-03-26 16:55:39 +0800

    collections
    part ni abi

--- src/main/java/financials/model/C_TransactionsModel.java
+++ src/main/java/financials/model/C_TransactionsModel.java
@@ -4,65 +4,64 @@ import java.sql.Date;
 
 public class C_TransactionsModel {
 	
-	private int C_OR_ID;
-	private String C_OR_No;
-	private String C_Type;
-	private String C_Payee;
-	private String C_TransDesc;
-	private Float C_Payment;
-	private Date C_Date_Paid;
-	private int Type_ID;
+	private int col_ORid;
+	private String col_ORno;
+	private String col_IGPname;
+	private String col_institune_name;
+	private String col_desc;
+	private Float col_amount;
+	private Date col_idate;
+	private int col_IGPid;
+	public int getCol_ORid() {
+		return col_ORid;
+	}
+	public void setCol_ORid(int col_ORid) {
+		this.col_ORid = col_ORid;
+	}
+	public String getCol_ORno() {
+		return col_ORno;
+	}
+	public void setCol_ORno(String col_ORno) {
+		this.col_ORno = col_ORno;
+	}
+	public String getCol_IGPname() {
+		return col_IGPname;
+	}
+	public void setCol_IGPname(String col_IGPname) {
+		this.col_IGPname = col_IGPname;
+	}
+	public String getCol_institune_name() {
+		return col_institune_name;
+	}
+	public void setCol_institune_name(String col_institune_name) {
+		this.col_institune_name = col_institune_name;
+	}
+	public String getCol_desc() {
+		return col_desc;
+	}
+	public void setCol_desc(String col_desc) {
+		this.col_desc = col_desc;
+	}
+	public Float getCol_amount() {
+		return col_amount;
+	}
+	public void setCol_amount(Float col_amount) {
+		this.col_amount = col_amount;
+	}
+	public Date getCol_idate() {
+		return col_idate;
+	}
+	public void setCol_idate(Date col_idate) {
+		this.col_idate = col_idate;
+	}
+	public int getCol_IGPid() {
+		return col_IGPid;
+	}
+	public void setCol_IGPid(int col_IGPid) {
+		this.col_IGPid = col_IGPid;
+	}
 	
-	public int getC_OR_ID() 
-	{
-		return C_OR_ID;
-	}
-	public void setC_OR_ID(int c_OR_ID) {
-		C_OR_ID = c_OR_ID;
-	}
-	public String getC_Type() {
-		return C_Type;
-	}
-	public void setC_Type(String c_Type) {
-		C_Type = c_Type;
-	}
-	public String getC_Payee() {
-		return C_Payee;
-	}
-	public void setC_Payee(String c_Payee) {
-		C_Payee = c_Payee;
-	}
-	public String getC_TransDesc() {
-		return C_TransDesc;
-	}
-	public void setC_TransDesc(String c_TransDesc) {
-		C_TransDesc = c_TransDesc;
-	}
-	public Float getC_Payment() {
-		return C_Payment;
-	}
-	public void setC_Payment(Float c_Payment) {
-		C_Payment = c_Payment;
-	}
 	
-	public int getType_ID() {
-		return Type_ID;
-	}
-	public void setType_ID(int type_ID) {
-		Type_ID = type_ID;
-	}
-	public Date getC_Date_Paid() {
-		return C_Date_Paid;
-	}
-	public void setC_Date_Paid(Date c_Date_Paid) {
-		C_Date_Paid = c_Date_Paid;
-	}
-	public String getC_OR_No() {
-		return C_OR_No;
-	}
-	public void setC_OR_No(String c_OR_No) {
-		C_OR_No = c_OR_No;
-	}
 	
 	
 	
