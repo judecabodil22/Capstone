836374cda338f4c9c91d3c6acec79e416bf751e6
ededdedry <evaldez2305@gmail.com>
2018-04-09 21:19:45 +0800

    JEV LIST small
    Date Format Fixed
    JEV No - clickable to view JEV Information
    Status - color change accordiing to status type
    search bar - search by JEV No

--- src/main/java/financials/controllers/financials_jevlistController.java
+++ src/main/java/financials/controllers/financials_jevlistController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,7 +18,7 @@ public class financials_jevlistController {
 	financials_jevlistDAO dao;
 	
 	@RequestMapping("financials_jevlist") //url
-	public ModelAndView jvl()
+	public ModelAndView jvl(@ModelAttribute("jevlist_delete") financials_jevlistModel model)
 	{
 		ModelAndView jvl = new ModelAndView();
 		jvl.addObject("jev_info", getJev_info());
@@ -29,5 +30,16 @@ public class financials_jevlistController {
 	public List<financials_jevlistModel> getJev_info() {
 		return dao.getJev_info();
 	}
+	
+	//DELETE FUNCTION
+	//@RequestMapping("financials_jevlist_delete")
+	//public ModelAndView jev_list_delete(@ModelAttribute("jevlist_delete") financials_jevlistModel model) {
+
+		//dao.delete(model);
+		//ModelAndView mav = new ModelAndView();
+		//mav.setViewName("redirect:financials_jevlist");
+		//return mav;
+
+	//}
 	
 }
